eb27a57932dbb636e56b2f0fac7ae9d59015d895
softwarepk <softwarepk@users.noreply.github.com>
2026-09-08 00:37:41 +0500

    Add installer cPanel preflight route

--- routes/install.php
+++ routes/install.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\InstallationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/install', [InstallationController::class, 'create'])->name('install.create');
+Route::post('/install/cpanel-check', [InstallationController::class, 'checkCpanel'])->name('install.cpanel-check');
 Route::post('/install', [InstallationController::class, 'store'])->name('install.store');
