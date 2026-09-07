cfb1d8d0b57d3160e2db405a3c2b3647bfcd7f28
softwarepk <softwarepk@users.noreply.github.com>
2026-09-07 17:41:52 +0500

    Make legacy install detection independently testable

--- config/installer.php
+++ config/installer.php
@@ -2,6 +2,7 @@
 
 return [
     'complete' => filter_var(env('INSTALLATION_COMPLETE', false), FILTER_VALIDATE_BOOL),
+    'legacy_configured' => null,
     'pending_file' => storage_path('app/installation.pending'),
     'complete_file' => storage_path('app/installation.complete'),
 ];
