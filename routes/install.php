bbac0e1656ff47c67a8a75ceec17b6ef3dfc56c5
softwarepk <softwarepk@users.noreply.github.com>
2026-09-08 01:31:05 +0500

    Route installer database preflight

--- routes/install.php
+++ routes/install.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/install', [InstallationController::class, 'create'])->name('install.create');
 Route::post('/install/cpanel-check', [InstallationController::class, 'checkCpanel'])->name('install.cpanel-check');
+Route::post('/install/database-check', [InstallationController::class, 'checkDatabase'])->name('install.database-check');
 Route::post('/install', [InstallationController::class, 'store'])->name('install.store');
