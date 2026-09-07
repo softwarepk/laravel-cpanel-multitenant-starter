d59ff6d15f3017bef302ec7286ebf69e7e75259a
softwarepk <softwarepk@users.noreply.github.com>
2026-09-07 19:29:32 +0500

    Add tenant deletion progress endpoint

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::prefix('central')
             Route::get('tenants/create', [CentralTenantPagesController::class, 'create'])->name('central.tenants.create');
             Route::post('tenants', [CentralTenantController::class, 'store'])->middleware('cpanel.provisioning')->name('central.tenants.store');
             Route::get('tenants/provisioning/{tenantId}', [CentralTenantController::class, 'provisioningStatus'])->name('central.tenants.provisioning.status');
+            Route::get('tenants/deletion/{tenantId}', [CentralTenantLifecycleController::class, 'deletionStatus'])->name('central.tenants.deletion.status');
             Route::get('tenants/{tenant}', [CentralTenantPagesController::class, 'show'])->name('central.tenants.show');
             Route::delete('tenants/{tenant}', [CentralTenantLifecycleController::class, 'destroy'])->name('central.tenants.destroy');
             Route::post('tenants/{tenant}/https/check', [CentralTenantController::class, 'checkPlatformHttps'])->name('central.tenants.https.check');
