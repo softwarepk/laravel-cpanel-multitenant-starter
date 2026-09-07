b68e26d89c4befa478a2e23b4727c21248b9c18a
softwarepk <softwarepk@users.noreply.github.com>
2026-09-07 17:21:16 +0500

    Fix PHPStan account user discovery

--- app/Services/InstallationDiscovery.php
+++ app/Services/InstallationDiscovery.php
@@ -73,7 +73,7 @@ class InstallationDiscovery
 
         if (function_exists('posix_geteuid') && function_exists('posix_getpwuid')) {
             $info = posix_getpwuid(posix_geteuid());
-            if (is_array($info) && isset($info['name']) && is_string($info['name'])) {
+            if (is_array($info)) {
                 return $info['name'];
             }
         }
