eb3d62bb538c55e56a9c10614a6a290d7a973fdf
softwarepk <softwarepk@users.noreply.github.com>
2026-09-07 16:46:21 +0500

    Route uninstalled deployments through first-run wizard

--- app/Http/Middleware/InitializeTenantFromHost.php
+++ app/Http/Middleware/InitializeTenantFromHost.php
@@ -3,16 +3,40 @@
 namespace App\Http\Middleware;
 
 use App\Models\Tenant;
+use App\Services\InstallationState;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class InitializeTenantFromHost
 {
+    public function __construct(private readonly InstallationState $installation) {}
+
     public function handle(Request $request, Closure $next): Response
     {
         $host = strtolower($request->getHost());
         $centralDomains = config('tenancy.central_domains', []);
+        $installerPath = $request->is('install') || $request->is('install/*');
+
+        if ($this->installation->requiresInstallation()) {
+            if ($request->is('up')) {
+                return $next($request);
+            }
+
+            if (! $request->isSecure()) {
+                return redirect()->away('https://'.$host.($installerPath ? $request->getRequestUri() : '/install'), 308);
+            }
+
+            return $installerPath ? $next($request) : redirect('/install');
+        }
+
+        if ($installerPath) {
+            if (in_array($host, $centralDomains, true)) {
+                return redirect('/central/login');
+            }
+
+            abort(404);
+        }
 
         if (in_array($host, $centralDomains, true)) {
             if ($request->is('up') || $request->is('central') || $request->is('central/*')) {
